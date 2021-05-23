edbe8d9ad11c692de0e8450b437ed34b1f5cc897
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2021-05-23 17:51:23 +0200

    - Used TreeSet instead of a regular Set as a queue. - Moved the possible answers to their own Set. - Added a shortcut to remove the state without adding its actions if one of them finds the target.

--- src/main/kotlin/dev/ahmedmourad/college/oral/ai/strategies/UCS.kt
+++ src/main/kotlin/dev/ahmedmourad/college/oral/ai/strategies/UCS.kt
@@ -1,6 +1,7 @@
 package dev.ahmedmourad.college.oral.ai.strategies
 
 import dev.ahmedmourad.college.oral.ai.*
+import java.util.*
 
 class UCS : Strategy {
     override fun findPath(
@@ -10,14 +11,20 @@ class UCS : Strategy {
     ): Answer {
 
         val visitedNodes = mutableSetOf<Node>()
-        val queue = mutableSetOf(initialState)
+
+        val queue = TreeSet<State> { s1, s2 ->
+            s1.totalCost.compareTo(s2.totalCost)
+        }
+
+        val possibleAnswers = mutableSetOf<State>()
+
+        queue.add(initialState)
 
         while (queue.isNotEmpty()) {
 
-            val currentState = queue
-                .filter { it.position != target }
-                .minByOrNull { it.totalCost }
-                ?: break
+            val currentState = queue.firstOrNull {
+                it.position != target
+            } ?: break
 
             queue.remove(currentState)
 
@@ -25,17 +32,22 @@ class UCS : Strategy {
                 .findAvailableActions(currentState)
                 .filter { it.target !in visitedNodes }
 
-            queue.addAll(availableActions.map {
-                currentState.takeAction(it)
-            })
+            val finishingMove = availableActions.firstOrNull { it.target == target }
+            if (finishingMove != null) {
+                possibleAnswers.add(currentState.takeAction(finishingMove))
+            } else {
+                queue.addAll(availableActions.map {
+                    currentState.takeAction(it)
+                })
+            }
 
             visitedNodes.add(currentState.position)
         }
 
-        val correct = queue
-            .minByOrNull { it.totalCost }
-            ?: return Answer(null)
+        val correct = possibleAnswers.minByOrNull {
+            it.totalCost
+        } ?: return Answer(null)
 
-        return Answer(correct, queue.minus(correct))
+        return Answer(correct, possibleAnswers.minus(correct))
     }
 }
